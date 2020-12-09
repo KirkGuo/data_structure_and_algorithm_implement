d00518e20a97a2be89d975b206594d5e6d22c553
KirkGuo <55413682+KirkGuo@users.noreply.github.com>
2020-12-09 14:41:41 -0500

    Update kmp.py

--- kmp.py
+++ kmp.py
@@ -1,12 +1,12 @@
 def make_next(pattern):
-    nxt = [-1] + [0 for _ in pattern]
+    nxt = [0 for _ in pattern]
     n = len(pattern)
     i, j = 0, -1
-    while i < n:
+    while i < n - 1:
         if j == -1 or pattern[i] == pattern[j]:
             i += 1
             j += 1
-            nxt[i] = j if pattern[i] != pattern[j] else nxt[j]
+            nxt[i] = j
         else:
             j = nxt[j]
     return nxt
@@ -22,6 +22,6 @@ def kmp(s, p):
         else:
             j = nxt[j]
 
-    if j >= len(p):
-        return i - len(p)
+    if j == len(p):
+        return i - j
     return -1
